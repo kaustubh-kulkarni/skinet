3fd539e7b5afaa402fb4221000bea7b25291182f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2022-08-22 16:05:04 +0200

    Included the product brands and types in the ProductRepository

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -24,12 +24,19 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                    .Include(p => p.ProductBrand)
+                    .Include(p => p.ProductType)
+                    .FirstOrDefaultAsync(p => p.Id == id);
         }
 
+        //We are adding ProductBrand and ProductTypes as Eager loading so we can see them in the request
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                    .Include(p => p.ProductBrand)
+                    .Include(p => p.ProductType)
+                    .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
